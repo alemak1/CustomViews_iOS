ca322db114f6d37531724bb39705ee88f89c02cb
yongrenwang <czie.alexmak@gmail.com>
2017-02-12 13:54:16 +0800

    Added a new CocoaTouch class (subclassing UIView) named TargetView

---
+++ ImageRiddle/TargetView.swift
@@ -0,0 +1,21 @@
+//
+//  TargetView.swift
+//  ImageRiddle
+//
+//  Created by Aleksander Makedonski on 2/12/17.
+//  Copyright © 2017 Builtlight. All rights reserved.
+//
+
+import UIKit
+
+class TargetView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
