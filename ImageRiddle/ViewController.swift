7c1a02affd9e018ad47746bfab9a973a165abb51
yongrenwang <czie.alexmak@gmail.com>
2017-02-12 13:52:05 +0800

    Started a new single-view application project; created initial UI in storyboard

---
+++ ImageRiddle/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ImageRiddle
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+     
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# CustomViewsSwift2.2
+Bjorn Chambless creating a workshop on Working with Custom Views in Swift 2.2 
